b8772de42108801ad281fe12347374a496621068
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-01 12:24:12 +0200

    Add supplier and implement query 2

---
+++ LINQ/Supplier.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace LINQ
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContactPerson { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        // One supplier can have many products
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
