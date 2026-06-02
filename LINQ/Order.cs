1a811b3880e1fb73978635cc78757413d7d63757
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-02 22:25:10 +0200

    Created Order model

---
+++ LINQ/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace LINQ
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public double TotalAmount { get; set; }
+        public string Status { get; set; } = null!;
+    }
+}
