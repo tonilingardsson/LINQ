888df7104163c3426c64747cb675cf71c7c67a71
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-02 22:24:29 +0200

    Created Product model

--- LINQ/Product.cs
+++ LINQ/Product.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public decimal Price { get; set; }
+        public double Price { get; set; }
         public int StockQuantity { get; set; }
 
         public int CategoryId { get; set; }
