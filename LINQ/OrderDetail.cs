e59c95d797b5038af0141ba12bcc2a3d0a5e2a69
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-02 22:25:22 +0200

    Created OrderDetail model

---
+++ LINQ/OrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LINQ
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+        public Order Order { get; set; } = null!;
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+
+        public int Quantity { get; set; }
+        public double UnitPrice { get; set; }
+
+    }
+}
