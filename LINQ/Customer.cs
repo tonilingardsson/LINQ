ea47f25ba5f0e5390eb02bdf64d9136747d7d490
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-02 22:24:07 +0200

    Created Customer model

--- LINQ/Customer.cs
+++ LINQ/Customer.cs
@@ -3,9 +3,9 @@
     public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
         public int Age { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
     }
 }
