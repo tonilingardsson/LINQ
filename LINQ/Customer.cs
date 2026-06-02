84f2478524a0e2339ef9635f292016fc652679aa
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-02 10:29:19 +0200

    Updated Customer with the data formats

---
+++ LINQ/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace LINQ
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
