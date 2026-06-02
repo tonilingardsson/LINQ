74546125f1b9a93c3a75d105f76f9bd0cb5536b6
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-02 22:24:56 +0200

    Updated Supplier model

--- LINQ/Supplier.cs
+++ LINQ/Supplier.cs
@@ -5,10 +5,10 @@ namespace LINQ
     public class Supplier
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string ContactPerson { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
+        public string Name { get; set; } = null!; 
+        public string ContactPerson { get; set; } = null!;  
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
 
         // One supplier can have many products
         public List<Product> Products { get; set; } = new List<Product>();
