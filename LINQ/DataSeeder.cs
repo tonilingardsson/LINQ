0eb0913c2209364ad3f7821ce8154bdbce405066
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-04 12:43:54 +0200

    Move seed logic to DataSeeder

---
+++ LINQ/DataSeeder.cs
@@ -0,0 +1,12 @@
+﻿using LINQ.Models;
+
+namespace LINQ
+{
+    public static class DataSeeder
+    {
+        public static void Seed(MyDbContext context)
+        {
+            // paste SeedData code here
+        }
+    }
+}
